69a0bf16bcf2eada919b8fb594117dbd43c6c07d
hsj <seungjune.hwang@gmail.com>
2022-08-12 15:02:23 +0900

    음식 추천 웹버전 플라스크

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/naver')
+def naver():
+    return '네이버 사이트'
+
+@app.route('/food')
+def food():
+    foodlist = ["짜장면", "짬뽕", "탕슉", "우동", "돈까스"]
+    foodname = random.choice(foodlist)
+    return render_template('food.html', data=foodname)
+
+if __name__ == '__main__':
+    app.run()
